e77e66cc8a1b94259794c98809d0fc82f066192e
Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
2018-01-24 10:28:48 +0100

    Ajout Police
    test

--- assets/QCM (63)/script.ts
+++ assets/QCM (63)/script.ts
@@ -2,13 +2,13 @@ class UIQCMBehavior extends UIButtonManagerBehavior {
   buttonAction(){
     switch(this.buttonIndex){
       case 0:
-        Sup.log("reponse 1");
+        Sup.log("Réponse 1");
         break;
       case 1:
-        Sup.log("reponse 2");
+        Sup.log("Réponse 2");
         break;
       case 2:
-        Sup.log("reponse 3");
+        Sup.log("Réponse 3");
         break;
       default:
         break;
