400d7ccc451203ebaf40d884f789ce89c6085449
Enzo Contini <enzo.contini@etu.univ-lyon1.fr>
2018-01-24 12:17:46 +0100

    Ajout Questions
    1->6

--- assets/TextFieldBehavior (65)/script.ts
+++ assets/TextFieldBehavior (65)/script.ts
@@ -59,18 +59,72 @@ class TextFieldBehavior extends Sup.Behavior {
     
     Sup.getActor("Question"+this.idQuest).setVisible(true);
     if(Sup.Input.wasKeyJustPressed("RETURN",{autoRepeat: false})){
-          if(this.text=="oui"){
-            this.text="";
-            
+        
+        //question 1
+        
+        if("Question"+this.idQuest=="Question1"&&this.text=="reserve"||this.text=="Reserve"||this.text=="RESERVE"||this.text=="book"||this.text=="Book"||this.text=="BOOK"){      
+          let player = Sup.Audio.playSound("Dialogue1"); 
+          player.play();
+          this.text="";
             Sup.getActor("Question"+this.idQuest).setVisible(false);
             Sup.getActor("Faux").setVisible(false)
             this.idQuest=this.idQuest+1;
-            
+            player.stop();
             this.refresh();
           }
+      
+      //question 2
+        
+        else if("Question"+this.idQuest=="Question2"&&this.text=="Booked"||this.text=="booked"||this.text=="BOOKED"){
+            this.text="";
+            Sup.getActor("Question"+this.idQuest).setVisible(false);
+            Sup.getActor("Faux").setVisible(false)
+            this.idQuest=this.idQuest+1;
+            this.refresh();
+          }
+      
+      //question 3
+        
+        else if("Question"+this.idQuest=="Question3"&&this.text=="full"||this.text=="Full"||this.text=="FULL"){
+            this.text="";
+            Sup.getActor("Question"+this.idQuest).setVisible(false);
+            Sup.getActor("Faux").setVisible(false)
+            this.idQuest=this.idQuest+1;
+            this.refresh();
+          }
+      
+      //question 4
+        
+        else if("Question"+this.idQuest=="Question4"&&this.text=="booking"||this.text=="Booking"||this.text=="BOOKING"){
+            this.text="";
+            Sup.getActor("Question"+this.idQuest).setVisible(false);
+            Sup.getActor("Faux").setVisible(false)
+            this.idQuest=this.idQuest+1;
+            this.refresh();
+          }
+      
+      //question 5
+        
+        else if("Question"+this.idQuest=="Question5"&&this.text=="Hold"||this.text=="hold"||this.text=="HOLD"||this.text=="Keep"||this.text=="keep"||this.text=="KEEP"){
+            this.text="";
+            Sup.getActor("Question"+this.idQuest).setVisible(false);
+            Sup.getActor("Faux").setVisible(false)
+            this.idQuest=this.idQuest+1;
+            this.refresh();
+          }
+      
+      //question 6
+        
+        else if("Question"+this.idQuest=="Question6"&&this.text=="deposit"||this.text=="Deposit"||this.text=="DEPOSIT"){
+            this.text="";
+            Sup.getActor("Question"+this.idQuest).setVisible(false);
+            Sup.getActor("Faux").setVisible(false)
+            this.idQuest=this.idQuest+1;
+            this.refresh();
+          }
+      
           else{
             this.text="";
-            
             Sup.getActor("Faux").setVisible(true);
             this.refresh();
           }
